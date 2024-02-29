04c87d7e8c0c36ca5ac3cb78ac448df250bc9f18
charan kosari <shivacharankosari099@gmail.com>
2024-02-29 20:03:38 +0530

    updated frontend

--- src/components/Home/DiyProductContainer.jsx
+++ src/components/Home/DiyProductContainer.jsx
@@ -6,7 +6,7 @@ function DiyProductProductContainer() {
 
     <div className='px-6 '>
     
-    <h1 className='ml-3 font-bold text-black lg:text-6xl text-2xl '>DiyProduct Robokits</h1>
+    <h1 className='ml-3 font-bold text-black lg:text-6xl text-2xl ' id='homw'>DiyProduct Robokits</h1>
     <div className='overflow-scroll' id='no-scrollbar'>
     <div className="carousel rounded-box">
   <div className="carousel-item">
