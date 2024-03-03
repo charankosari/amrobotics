f4525c9b217fbe5bb078824e4da57f2e363a48b7
srinath <iamsrinath5255@gmail.com>
2024-03-03 15:04:23 +0530

    added address feature

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -16,6 +16,7 @@ import ForgotPassword from "../profileTabsDetailview/ForgotPassword.jsx";
 import ContactUs from "../profileTabsDetailview/ContactUs.jsx";
 import Faq from '../profileTabsDetailview/FAQ.jsx'
 
+
 const tabIds={
 account:"ACCOUNT",
 order:"ORDER",
@@ -52,18 +53,14 @@ function Profile() {
     case tabIds.contact:
       return <ContactUs/>
     case tabIds.faqs:
-<<<<<<< HEAD
-      return <Faq/>   
-=======
       return <Faq/>
-    
+ 
    
->>>>>>> e113a60d252eb9240cc854546f686029f13b0a86
      }
   }
 
     return (
-      <div className="bg-white h-auto pb-12 overflow-auto ">
+      <div className="bg-white h-auto pb-12">
         <Navbar />
         <div className="flex flex-row  mt-12 h-[90dvh]  " id="some">
           <div className="flex flex-col gap-12 mx-12 w-[30%]" id="gap">
@@ -143,11 +140,20 @@ function Profile() {
                 </p>
               </div>
             </div>
-<<<<<<< HEAD
-            
-=======
-           
->>>>>>> e113a60d252eb9240cc854546f686029f13b0a86
+            <div id={tabIds.Resetpassword} onClick={()=>{changeActiveId(tabIds.Resetpassword)}}  className="flex cursor-pointer flex-row gap-2 mx-4 border-b-2 border-b-zinc-400 pb-2">
+            <MdLockReset 
+                className="w-[90px] h-[90px] mr-3 text-[#FF9F1C]"
+                id="icon"
+              />
+              <div className="flex flex-col">
+                <h1 className="text-5xl" id="head">
+                  Reset your passwpord
+                </h1>
+                <p className="text-2xl w-[70%]" id="head-text">
+                  Reset password if you forgot your old password
+                </p>
+              </div>
+            </div>
             <div className="flex flex-row gap-2 mx-4 border-b-2 border-b-zinc-400 pb-2 cursor-pointer"  id={tabIds.contact} onClick={()=>{changeActiveId(tabIds.contact)}}>
               <RiCustomerServiceLine
                 className="w-[90px] h-[90px] mr-3 text-[#FF9F1C] "
