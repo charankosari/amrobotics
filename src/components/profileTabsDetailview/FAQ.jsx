9e4536d5a9f29cf74e561479c0330537ac3ded6e
charan kosari <shivacharankosari099@gmail.com>
2024-03-03 13:28:57 +0530

    updated frontend

--- src/components/profileTabsDetailview/FAQ.jsx
+++ src/components/profileTabsDetailview/FAQ.jsx
@@ -22,12 +22,25 @@ function Faq() {
             <h1 className='text-black font-black text-2xl'>FAQ'S</h1>
             <div >
                 {object.map((item, index) => (
-                    <div key={index} className=' gap-2 flex flex-col bg-[#ceced2] rounded-md p-2 w-auto mb-2 'id='thediv'>
-                        <h1 className='text-3xl' id='remove'>{item.question}</h1>
-                        <p className='text-xl' id='remove1'>{item.answer}</p>
+                    <div className="collapse collapse-plus " key={index}>
+                    <input type="radio" name="my-accordion-3" defaultChecked /> 
+                    <div className="collapse-title text-xl font-medium">
+                    {item.question}
                     </div>
+                    <div className="collapse-content"> 
+                      <p>{item.answer}</p>
+                    </div>
+                  </div>
                 ))}
             </div>
+
+
+           
+
+
+
+
+
         </div>
     );
 }
