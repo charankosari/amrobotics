3108c0361977160431f470209a2fc4ac803def99
unknown <shivacharankosari099@gmail.com>
2024-03-29 10:00:07 +0530

    admin page

--- src/ProtectedRoutes/Sidebar.tsx
+++ src/ProtectedRoutes/Sidebar.tsx
@@ -1,7 +1,11 @@
 import { useState } from "react";
+import logo from "./logo.svg";
 import "./AdminStyles.css";
-import logo from './logo.svg'
-const navItems = ["home", "settings", "backup", "mail", "cloud", "layers"];
+import { FaUsers } from "react-icons/fa";
+import { RxCross1 } from "react-icons/rx";
+import { IoMdMenu } from "react-icons/io";
+
+// const navItems = ["home", "settings", "backup", "mail", "cloud", "layers"];
 
 export const Sidebar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -9,24 +13,25 @@ export const Sidebar = () => {
     <nav className={`sidebar ${isOpen ? "open" : ""}`}>
       <div className="sidebar-inner">
         <header className="sidebar-header">
-          <button id="button"
+          <button
             type="button"
             className="sidebar-burger"
             onClick={() => setIsOpen(!isOpen)}
           >
             <span className="material-symbols-outlined">
-              {isOpen ? "close" : "menu"}
+              {isOpen ? <RxCross1/> : <IoMdMenu/>}
             </span>
           </button>
-          <img src={logo} className="sidebar-logo" />
+          {/* <img src={logo} className="sidebar-logo" /> */}
+          <span>AM ROBOTICS</span>
         </header>
         <nav className="sidebar-menu">
-          {navItems.map((item) => (
-            <button key={item} type="button" className="sidebar-button" id="button">
-              <span className="material-symbols-outlined">{item}</span>
-              <p>{item}</p>
+          {/* {navItems.map((item) => ( */}
+            <button  type="button" className="sidebar-button">
+              <span className="material-symbols-outlined"><FaUsers/></span>
+              <p>users</p>
             </button>
-          ))}
+          {/* ))} */}
         </nav>
       </div>
     </nav>
