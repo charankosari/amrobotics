6af45b1b6857ff428af7001807425665faeff299
charan kosari <shivacharankosari099@gmail.com>
2024-04-12 19:58:15 +0530

    updated product search

--- src/ProtectedRoutes/AdminProducts.jsx
+++ src/ProtectedRoutes/AdminProducts.jsx
@@ -5,6 +5,12 @@ import "./AdminStyles.css"
 const AdminProducts = () => {
   
   const [products, setProducts] = useState([]);
+  const [filteredProduct,setFilteredProducts]=useState([]);
+  const [search,setSearch]=useState('');
+  
+
+ 
+  
   //fetching all products
   const fetchProducts = async () => {
     try {
@@ -24,6 +30,23 @@ const AdminProducts = () => {
   useEffect(() => {
     fetchProducts();
   }, []);
+
+  //searching for a product
+  const searchProduct = () => {
+    const filteredProduct = products.filter(product => {
+      const searchLower = search.toLowerCase();
+      return product.name.toLowerCase().includes(searchLower) || product.category.toLowerCase().includes(searchLower);
+    });
+    setFilteredProducts(filteredProduct);
+  };
+
+  //clearing search
+  
+  const clearSearch = () => {
+    setSearch('');
+    setFilteredProducts([]);
+  };
+
 
   //deleting specific product
   
@@ -140,8 +163,10 @@ const AdminProducts = () => {
       <div className='flex sm:flex-row flex-col sm:gap-12 gap-1 ml-12 sm:ml-0 items-center '> 
       <div><h2 className="text-2xl font-bold mb-2">Product Details</h2></div>
       <div className='flex flex-row gap-1 justify-center items-center'>
-        <input type="text" className='w-[200px] h-[34px] p-4 bg-white text-black rounded-md border' placeholder='enter name or email' />
-        <button className='bg-blue-500 p-[5px] rounded-md text-white'>search</button>
+        <input type="text" className='w-[200px] h-[34px] p-4 bg-white text-black rounded-md border ' onChange={(e)=>{setSearch(e.target.value)}} value={search}  placeholder='enter product or category' />
+        <button className='bg-blue-500 p-[5px] rounded-md text-white' onClick={searchProduct}>search</button>
+        <button className='bg-yellow-500 p-[5px] rounded-md text-white'  onClick={clearSearch}>clear</button>
+
       </div>
       </div>
      
@@ -150,7 +175,7 @@ const AdminProducts = () => {
       <div>
         <button className='btn bg-green-600 text-white mb-2' onClick={() => setShowModal2(true)}>Add Product</button>
         <ul>
-          {products.map(product => (
+        {(search ? filteredProduct : products).map((product) => (
             <li key={product.id} className="flex items-center justify-between border-b py-2">
               <div>
                 <p className="text-lg">{product.name}</p>
