e3aa22adb45a4b0a5a31e1e17e49a51f73cbbe6e
srinath <iamsrinath5255@gmail.com>
2024-05-02 17:42:05 +0530

    update

--- src/components/MarketPlace/SpecificProduct.jsx
+++ src/components/MarketPlace/SpecificProduct.jsx
@@ -4,7 +4,11 @@ import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
 import "./Specificproduct.css";
 import { useNavigate, useParams } from "react-router-dom";
-import { GetDetailProduct, getWishlist, sendToWishlistApi } from "../../helper.js";
+import {
+  GetDetailProduct,
+  getWishlist,
+  sendToWishlistApi,
+} from "../../helper.js";
 import { ThreeDots } from "react-loader-spinner";
 import { sendToCartApi } from "../../helper.js/";
 import { ToastContainer, toast } from "react-toastify";
@@ -13,9 +17,8 @@ import { IoIosHeart } from "react-icons/io";
 import { createReview } from "../../helper.js";
 
 export default function MyComponent() {
-  const navigate=useNavigate();
- 
-  
+  const navigate = useNavigate();
+
   const renderStars = (rating) => {
     let stars = "";
     for (let i = 0; i < rating; i++) {
@@ -28,7 +31,7 @@ export default function MyComponent() {
   const [details, setDetails] = useState("");
   const [loading, setLoading] = useState(true);
   const [cartCount, setCartCount] = useState(1);
-  const [colorr,setColor]=useState('grey')
+  const [colorr, setColor] = useState("grey");
   const [reviewDetails, setreviewDetails] = useState({
     rating: 0,
     comment: "",
@@ -36,10 +39,9 @@ export default function MyComponent() {
   });
   const [showreview, setshowreview] = useState(false);
 
-
   useEffect(() => {
     getDetails();
-   }, []);
+  }, []);
 
   const handleImageClick = (image) => {
     setMainImage(image);
@@ -50,50 +52,47 @@ export default function MyComponent() {
     setActiveSection(section);
   };
   const getDetails = async () => {
-    console.log("hello");
     const response = await GetDetailProduct(id);
     const response2 = await getWishlist();
-  
+
     if (response.status === 200) {
       setDetails(response.data.product);
       console.log(response.data.product);
       setMainImage(response.data.product.images[0]);
-  
+
       if (response2.status === 200) {
         // Initialize dataSet
         let dataSet = new Set();
-  
+
         // Populate dataSet with names from response2
         for (let i = 0; i < response2.data.data.length; i++) {
           dataSet.add(response2.data.data[i].name);
         }
         console.log(dataSet);
-        console.log(response.data.product.name)
-  
+        console.log(response.data.product.name);
+
         // Check if details.name is present in dataSet
         if (dataSet.has(response.data.product.name)) {
-          setColor('red');
-          console.log('red');
+          setColor("red");
+          console.log("red");
         } else {
-          setColor('grey');
+          setColor("grey");
         }
       }
     }
-  
+
     setLoading(false);
   };
 
-
-
   const sendToCart = async () => {
     const response = await sendToCartApi({ id, cartCount });
-    console.log(response)
+    console.log(response);
     if (response.status == 200) {
-      console.log("product added to cart ")
+      console.log("product added to cart ");
       toast.success("Product added to cart");
     }
   };
-  
+
   const BuyNowButton = async () => {
     const response = await sendToCartApi({ id, cartCount });
     console.log(response);
@@ -102,19 +101,20 @@ export default function MyComponent() {
       toast.success("Product added to cart");
       setTimeout(() => {
         navigate("/profile", { state: { tabId: "CART" } });
-      }, 2000); // 2000 milliseconds = 2 seconds
+      }, 2000); 
     }
   };
-  
 
   const wishListItem = async () => {
     const response = await sendToWishlistApi(id);
     console.log(response);
     if (response.status == 200) {
-      // setLoveClicked(!isLoveClicked);
       getDetails();
-      toast.success("Item Wishlisted");
-
+      if (response.data.message === "Product wishlisted successfully") {
+        toast.success("Item Wishlisted");
+      } else {
+        toast.warning("Item removed from Wishlisted");
+      }
     }
   };
 
@@ -125,7 +125,7 @@ export default function MyComponent() {
     if (response.status == 200) {
       getDetails();
       toast.success("Product reviewed successfully");
-      window.location.reload()
+      window.location.reload();
     }
   };
 
@@ -199,7 +199,7 @@ export default function MyComponent() {
                             key={index}
                             loading="lazy"
                             srcSet={image}
-                            className="aspect-[1.14] object-contain object-center w-[128px] overflow-hidden shrink-0 max-w-full grow max-md:mt-3 cursor-pointer "
+                            className={`aspect-[1.14] object-contain object-center w-[128px] overflow-hidden shrink-0 max-w-full grow max-md:mt-3 cursor-pointer`}
                             onClick={() => handleImageClick(image)}
                           />
                         ))}
@@ -214,7 +214,8 @@ export default function MyComponent() {
                     {details.name}
                   </div>
                   <div className="text-zinc-800   text-sm  mt-7 self-start ">
-                  {"⭐".repeat(details.rating)}{`${details.noOfReviews} reviews`}
+                    {"⭐".repeat(details.rating)}
+                    {`${details.noOfReviews} reviews`}
                   </div>
                   <div className="flex items-stretch gap-4 mt-3 self-start">
                     <div className="text-neutral-700 text-sm whitespace-nowrap bg-zinc-300 bg-opacity-70 grow justify-center items-stretch px-3.5 py-1.5 rounded-xl">
@@ -227,11 +228,13 @@ export default function MyComponent() {
                         {details.price}
                       </div>
                       <div className="text-red-700 text-3xl font-semibold line-through">
-                      {Math.floor(details.price + details.price * (15 + Math.random() * 5) / 100)}
+                        {Math.floor(
+                          details.price +
+                            (details.price * (15 + Math.random() * 5)) / 100
+                        )}
                       </div>
                     </div>
-                  
-                  </div> 
+                  </div>
 
                   <div className="flex flex-row items-center mt-4">
                     <label>Quantity : </label>
@@ -252,25 +255,25 @@ export default function MyComponent() {
                     </select>
                   </div>
 
-                 
-                    <button
-                      onClick={() => {
-                     
-                        sendToCart();
-                      }}
-                      id="button-to-change"
-                      className="text-white sm:text-4xl text-xl font-semibold bg-neutral-700 mt-6 rounded-xl max-md:max-w-full max-md:px-5"
-                    >
-                      Add to cart
-                    </button>
+                  <button
+                    onClick={() => {
+                      sendToCart();
+                    }}
+                    id="button-to-change"
+                    className="text-white sm:text-4xl text-xl font-semibold bg-neutral-700 mt-6 rounded-xl max-md:max-w-full max-md:px-5"
+                  >
+                    Add to cart
+                  </button>
 
-                    <button
-                      id="button-to-change"
-                      onClick={()=>{BuyNowButton()}}
-                      className="text-white sm:text-4xl text-xl font-semibold bg-amber-500 self-stretch justify-center items-center mt-6 rounded-xl max-md:max-w-[400px] max-md:px-5"
-                    >
-                      Buy Now
-                    </button>
+                  <button
+                    id="button-to-change"
+                    onClick={() => {
+                      BuyNowButton();
+                    }}
+                    className="text-white sm:text-4xl text-xl font-semibold bg-amber-500 self-stretch justify-center items-center mt-6 rounded-xl max-md:max-w-[400px] max-md:px-5"
+                  >
+                    Buy Now
+                  </button>
                   <div className="text-zinc-800   text-sm whitespace-nowrap mt-5">
                     <span className="font-semibold text-base p-0">
                       <span className="text-amber-500  text-[1rem] p-0">
@@ -283,11 +286,13 @@ export default function MyComponent() {
               </div>
             </div>
           </div>
-          <div className="flex flex-row gap-[10px]  sm:gap-12 mt-4    "
-          id="a1j"
-          style={{marginLeft:'40px'}}>
+          <div
+            className="flex flex-row gap-[10px]  sm:gap-12 mt-4    "
+            id="a1j"
+            style={{ marginLeft: "40px" }}
+          >
             <div
-              className={` bg-[#c6cbc6] p-2 rounded-md hover:bg-[#a9aca9] cursor-pointer   ${ 
+              className={` bg-[#c6cbc6] p-2 rounded-md hover:bg-[#a9aca9] cursor-pointer   ${
                 activeSection === "description"
                   ? "text-black "
                   : "text-[#6e7271]"
@@ -296,14 +301,11 @@ export default function MyComponent() {
             >
               Description
             </div>
-            <div  
+            <div
               className={` bg-[#c6cbc6] p-2 rounded-md hover:bg-[#a9aca9] cursor-pointer  ${
-                activeSection === "insideBox"
-                ? "text-black "
-                : "text-[#6e7271]"
+                activeSection === "insideBox" ? "text-black " : "text-[#6e7271]"
               } text-[12px] sm:text-xl font-semibold   `}
               onClick={() => handleSectionClick("insideBox")}
-              
             >
               Inside Box
             </div>
@@ -311,8 +313,8 @@ export default function MyComponent() {
             <div
               className={` bg-[#c6cbc6] p-2 rounded-md hover:bg-[#a9aca9] cursor-pointer  ${
                 activeSection === "productDetails"
-                ? "text-black "
-                : "text-[#6e7271]"
+                  ? "text-black "
+                  : "text-[#6e7271]"
               } text-[12px] sm:text-xl font-semibold `}
               onClick={() => handleSectionClick("productDetails")}
             >
@@ -324,7 +326,7 @@ export default function MyComponent() {
             <div
               className="text-black sm:text-xl  mt-4 mx-2 ml-[22px] sm:ml-[44px]"
               id="text-to-be-expanded"
-              style={{fontSize:'0.875rem '}}
+              style={{ fontSize: "0.875rem " }}
             >
               {details.description}
             </div>
@@ -334,10 +336,10 @@ export default function MyComponent() {
             <div
               className="text-black sm:text:xl text-sm  mt-4 mx-2 ml-[22px] sm:ml-[44px] "
               id="text-to-be-expanded "
-              style={{fontSize:'0.875rem '}}
+              style={{ fontSize: "0.875rem " }}
             >
               consectetur adipisicing elit. Voluptates obcaecati nesciunt
-              repudiandae ut, id qsxsx  
+              repudiandae ut, id qsxsx
             </div>
           )}
 
@@ -345,12 +347,11 @@ export default function MyComponent() {
             <div
               className="text-black sm:text:xl text-sm  mt-4 mx-2 ml-[22px] sm:ml-[44px]"
               id="text-to-be-expanded"
-              style={{fontSize:'0.875rem ' }}
+              style={{ fontSize: "0.875rem " }}
             >
               elit. Voluptates obcaecati nesciunt repudiandae ut, id quis ex
               sunt vel earum dolores quisquam, eligendi impedit laboriosam
-              blanditiis dolorum 
-              nesciunt?
+              blanditiis dolorum nesciunt?
             </div>
           )}
 
@@ -360,7 +361,7 @@ export default function MyComponent() {
             </h1>
             <h3 className="text-xl mb-2 text-black font-semibold" id="glo">
               {" "}
-              {details.noOfReviews}  ratings
+              {details.noOfReviews} ratings
             </h3>
             {showreview ? (
               ""
@@ -376,42 +377,115 @@ export default function MyComponent() {
             )}
 
             {/* review form _____________*/}
-            {
-              showreview ?
-                <div className="form-container relative">
-                  <button onClick={() => { setshowreview(!showreview) }} className="absolute top-3 right-3 "><IoCloseSharp className="text-xl" /></button>
-                  <div className="logo-container">
-                    Create Review
-                  </div>
+            {showreview ? (
+              <div className="form-container relative">
+                <button
+                  onClick={() => {
+                    setshowreview(!showreview);
+                  }}
+                  className="absolute top-3 right-3 "
+                >
+                  <IoCloseSharp className="text-xl" />
+                </button>
+                <div className="logo-container">Create Review</div>
 
-                  <form className="form">
-                    <div className="form-group">
-                      <label htmlFor="review font-bold">Comment</label>
-                      <textarea onChange={(event) => setreviewDetails((prev => ({ ...prev, comment: event.target.value })))} rows={10} className="border p-2" id="reivew" placeholder="Write your experience of product.....">
+                <form className="form">
+                  <div className="form-group">
+                    <label htmlFor="review font-bold">Comment</label>
+                    <textarea
+                      onChange={(event) =>
+                        setreviewDetails((prev) => ({
+                          ...prev,
+                          comment: event.target.value,
+                        }))
+                      }
+                      rows={10}
+                      className="border p-2"
+                      id="reivew"
+                      placeholder="Write your experience of product....."
+                    ></textarea>
 
-                      </textarea>
+                    <div className="flex flex-row items-center mt-4">
+                      <label className="text-bold text-md mt-3">
+                        Rating :{" "}
+                      </label>
 
-                      <div className="flex flex-row items-center mt-4">
-                        <label className="text-bold text-md mt-3">Rating : </label>
-
-                        <div className="rating">
-                          <input value={1}  onClick={(event) => setreviewDetails((prev) => ({ ...prev, rating: event.target.value }))} type="radio" name="rating-2" className="mask mask-star-2 w-10 bg-orange-400" />
-                          <input value={2}  onClick={(event) => setreviewDetails((prev) => ({ ...prev, rating: event.target.value }))} type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400"/>
-                          <input value={3}  onClick={(event) => setreviewDetails((prev) => ({ ...prev, rating: event.target.value }))} type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                          <input value={4}  onClick={(event) => setreviewDetails((prev) => ({ ...prev, rating: event.target.value }))} type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                          <input value={5}  onClick={(event) => setreviewDetails((prev) => ({ ...prev, rating: event.target.value }))} type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                        </div>
-
-
+                      <div className="rating">
+                        <input
+                          value={1}
+                          onClick={(event) =>
+                            setreviewDetails((prev) => ({
+                              ...prev,
+                              rating: event.target.value,
+                            }))
+                          }
+                          type="radio"
+                          name="rating-2"
+                          className="mask mask-star-2 w-10 bg-orange-400"
+                        />
+                        <input
+                          value={2}
+                          onClick={(event) =>
+                            setreviewDetails((prev) => ({
+                              ...prev,
+                              rating: event.target.value,
+                            }))
+                          }
+                          type="radio"
+                          name="rating-2"
+                          className="mask mask-star-2 bg-orange-400"
+                        />
+                        <input
+                          value={3}
+                          onClick={(event) =>
+                            setreviewDetails((prev) => ({
+                              ...prev,
+                              rating: event.target.value,
+                            }))
+                          }
+                          type="radio"
+                          name="rating-2"
+                          className="mask mask-star-2 bg-orange-400"
+                        />
+                        <input
+                          value={4}
+                          onClick={(event) =>
+                            setreviewDetails((prev) => ({
+                              ...prev,
+                              rating: event.target.value,
+                            }))
+                          }
+                          type="radio"
+                          name="rating-2"
+                          className="mask mask-star-2 bg-orange-400"
+                        />
+                        <input
+                          value={5}
+                          onClick={(event) =>
+                            setreviewDetails((prev) => ({
+                              ...prev,
+                              rating: event.target.value,
+                            }))
+                          }
+                          type="radio"
+                          name="rating-2"
+                          className="mask mask-star-2 bg-orange-400"
+                        />
                       </div>
-
-
                     </div>
-                    <button onClick={submitReview} className="form-submit-btn" type="submit">Create Review</button>
-                  </form>
-
-                </div> : ""
-            }
+                  </div>
+                  <button
+                    onClick={submitReview}
+                    className="form-submit-btn"
+                    type="submit"
+                  >
+                    Create Review
+                  </button>
+                </form>
+              </div>
+            ) : (
+              ""
+            )}
 
             {/* reviews  */}
             <div className="flex flex-col gap-2">
@@ -422,8 +496,11 @@ export default function MyComponent() {
                   id="reviews"
                 >
                   <div className="flex flex-row items-center">
-                    <div className="w-[3rem] rounded-full mb-2  bg-black flex justify-center items-center" id="userimg">
-                <p className="text-white"> {(each.name).slice(0,1)}</p>
+                    <div
+                      className="w-[3rem] rounded-full mb-2  bg-black flex justify-center items-center"
+                      id="userimg"
+                    >
+                      <p className="text-white"> {each.name.slice(0, 1)}</p>
                     </div>
                     <h1
                       className="text-2xl ml-2 mb-1 font-[500] text-black"
@@ -438,7 +515,6 @@ export default function MyComponent() {
                   <p className="text-xl text-black mb-2" id="comments">
                     {each.comment}
                   </p>
-                 
                 </div>
               ))}
             </div>
